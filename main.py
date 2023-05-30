8b59aab7abb2d592e5ef1c49933e3dba91d782fd
JMBorines14 <jamesborines@gmail.com>
2023-05-30 22:50:45 +0800

    Modified NetCDF output + little tweaks

--- main.py
+++ main.py
@@ -11,50 +11,65 @@ def y_coord(idx):
     return d*idx + e*idx + yoff
 
 def simulation(L, D, I, depth_var, m, n):
+    """Main driver function for the simulation of the model
+    to the input dataset. This function takes the following parameters:
+    L => Matrix corresponding to the sum of S and D
+    D => Depth matrix
+    I => I_total matrix
+    depth_var => netcdf variable where output will be added
+    m, n => dimensions of the matrices L, D, and I"""
+
+    R = np.zeros((m, n)) #initialize the rainfall function
     for time in range(1):
-        #update R by calling update_R
-        #update D
+        D += R
         I, ibabawas, idadagdag = update_D(L, D, I, m, n)
         D = D - ibabawas + idadagdag
 
         depth_var[time, :, :] = D
         L = S + D
+        R = np.zeros((m, n)) #update this to the rainfall function
 
-#initialize the dataset
-dataset = gdal.Open(sys.argv[1])
-n, m, band = dataset.RasterXSize, dataset.RasterYSize, dataset.RasterCount
-print(f'The size is {m} x {n}')
-xoff, a, b, yoff, d, e = dataset.GetGeoTransform()
+if __name__ == "__main__":
+    #open the TIFF file
+    dataset = gdal.Open(sys.argv[1])
+    n, m, band = dataset.RasterXSize, dataset.RasterYSize, dataset.RasterCount
+    xoff, a, b, yoff, d, e = dataset.GetGeoTransform()
 
-#initialize output file and dimensions
-output_dataset = nc.Dataset('output.nc', 'w', format = 'NETCDF4')
-time = output_dataset.createDimension('time', None)
-lat = output_dataset.createDimension('lat', m)
-lon = output_dataset.createDimension('lon', n)
+    #initialize output file and dimensions
+    output_dataset = nc.Dataset('output.nc', 'w', format = 'NETCDF4')
+    time = output_dataset.createDimension('time', None)
+    lat = output_dataset.createDimension('lat', m)
+    lon = output_dataset.createDimension('lon', n)
 
-times = output_dataset.createVariable('time', 'f8', ('time',))
-lats = output_dataset.createVariable('lat', 'f4', ('lat',))
-lons = output_dataset.createVariable('lon', 'f4', ('lon',))
-depth = output_dataset.createVariable('depth', 'f4', ('time', 'lat', 'lon',))
+    times = output_dataset.createVariable('time', 'f8', ('time',))
+    lats = output_dataset.createVariable('lat', 'f4', ('lat',))
+    lons = output_dataset.createVariable('lon', 'f4', ('lon',))
+    depth = output_dataset.createVariable('depth', 'f4', ('time', 'lat', 'lon',))
+    depth.grid_mapping = 'phlvfour'
 
-#output latitudes and longitudes to the dataset
-lats[:] = np.array([x_coord(k) for k in m])
-lons[:] = np.array([y_coord(k) for k in n])
+    #output latitudes and longitudes to the dataset
+    lats[:] = np.array([x_coord(k) for k in range(m)])
+    lons[:] = np.array([y_coord(k) for k in range(n)])
 
-#get surface matrix
-data1 = dataset.GetRasterBand(1).ReadAsArray()
-S = np.array([list(i) for i in list(data1)])
-dataset = None
+    #get surface matrix
+    data1 = dataset.GetRasterBand(1).ReadAsArray()
+    S = np.array([list(i) for i in list(data1)])
+    dataset = None
 
-#initialize rainfall matrix
-R = np.zeros((m,n)) #rainfall
-R[4093][4093] = 100
+    #initialize rainfall matrix
+    R = np.zeros((m,n)) #rainfall
+    R[4093][4093] = 100
 
-#define the I and D matrices
-I = np.zeros((m,n)) #I_total
-L = S
-D = R
+    #define the I and D matrices
+    I = np.zeros((m,n)) #I_total
+    D = np.zeros((m,n))
+    L = S + D
 
-#call to simulation function
-#simulation(C, L, D, I, depth, m, n)
-output_dataset.close()
+    #call to simulation function
+    simulation(L, D, I, depth, m, n)
+
+    #create a NetCDF Variable
+    phlvfour = output_dataset.createVariable('phlvfour', 'c')
+    phlvfour.spatial_ref = dataset.GetProjectionRef()
+    phlvfour.GeoTransform = " ".join(str(x) for x in dataset.GetGeoTransform())
+    output_dataset.close()
